33e910cf7411ed11c2b60196c88ce74977f8665d
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-05-21 08:39:53 +0800

    bruhv

---
+++ 2022/adv-web-dev/2005-vue/sandbox/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="app">
+		<JsonTodo
+			v-for="(todo, index) in todos"
+			:key="index"
+			:id="todo.id"
+			:userId="todo.userId"
+			:title="todo.title"
+			:completed="todo.completed"
+		/>
+	</div>
+</template>
+
+<script>
+import JsonTodo from './components/JsonTodo.vue';
+
+export default {
+	name: 'App',
+	components: {
+		JsonTodo,
+	},
+
+	created() {
+		const getData = async (x) => {
+			const res = await fetch(
+				`https://jsonplaceholder.typicode.com/todos/${x}`
+			);
+			const json = await res.json();
+			return json;
+		};
+		const promises = [];
+		for (let i = 1; i <= 15; i++) {
+			promises.push(getData(i));
+		}
+		Promise.all(promises).then((res) => {
+			this.todos = res;
+		});
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ 2022/adv-web-dev/2005-vue/sandbox/src/components/JsonTodo.vue
@@ -0,0 +1,26 @@
+<template>
+	<div :class="{ completed }">
+		<h1>{{ id }}: {{ title }} ({{ userId }})</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'JsonTodo',
+	props: ['id', 'userId', 'title', 'completed'],
+};
+</script>
+
+<style scoped>
+div {
+	text-align: left;
+	background-color: #eee;
+	padding: 20px;
+	margin: 20px;
+	border: red 2px solid;
+}
+
+.completed {
+	border: green 2px solid;
+}
+</style>
